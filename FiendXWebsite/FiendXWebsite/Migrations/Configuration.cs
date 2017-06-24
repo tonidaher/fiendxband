79d7a9ed0cece228788f280831ff77e524c9f466
tonidaher <dahertony@hotmail.com>
2017-06-24 14:41:42 -0400

    project configuration

---
+++ FiendXWebsite/FiendXWebsite/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace FiendXWebsite.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FiendXWebsite.Models.TestClass>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(FiendXWebsite.Models.TestClass context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
